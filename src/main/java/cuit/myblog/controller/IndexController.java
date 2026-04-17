9f470cc1a545806e3cab5cc82c2d20014abdf34a
A <2978131465@qq.com>
2026-04-17 11:11:39 +0800

    修复bug

---
+++ src/main/java/cuit/myblog/controller/IndexController.java
@@ -0,0 +1,14 @@
+package cuit.myblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        // 返回 "forward:/index.html" 告诉 Spring Boot 转向静态资源文件夹下的 index.html
+        return "forward:/index.html";
+    }
+}
